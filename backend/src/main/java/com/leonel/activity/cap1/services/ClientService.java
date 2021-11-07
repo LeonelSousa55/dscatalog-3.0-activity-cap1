f11ddd1b840a9b6f9c8639a4c250ea1ae243ca33
Leonel <leonel55sousa@gmail.com>
2021-11-07 10:22:43 -0300

    feat: add delete client

--- backend/src/main/java/com/leonel/activity/cap1/services/ClientService.java
+++ backend/src/main/java/com/leonel/activity/cap1/services/ClientService.java
@@ -5,11 +5,14 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.leonel.activity.cap1.services.exceptions.DatabaseException;
 import com.leonel.activity.cap1.dto.ClientDTO;
 import com.leonel.activity.cap1.entities.Client;
 import com.leonel.activity.cap1.repositories.ClientRepository;
@@ -61,6 +64,20 @@ public class ClientService {
 		} catch (EntityNotFoundException e) {
 
 			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+
+		try {
+
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+
+			throw new ResourceNotFoundException("Id not found " + id);
+		} catch (DataIntegrityViolationException e) {
+
+			throw new DatabaseException("Entegrity vionation");
 		}
 	}
 
