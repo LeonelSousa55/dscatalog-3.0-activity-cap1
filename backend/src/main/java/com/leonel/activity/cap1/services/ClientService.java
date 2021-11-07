487ffb16b26fdbc4c4e40c8686e0dcb4bd14b3d4
Leonel <leonel55sousa@gmail.com>
2021-11-07 10:13:04 -0300

    feat: add new client and update client

--- backend/src/main/java/com/leonel/activity/cap1/services/ClientService.java
+++ backend/src/main/java/com/leonel/activity/cap1/services/ClientService.java
@@ -2,6 +2,8 @@ package com.leonel.activity.cap1.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -18,7 +20,7 @@ public class ClientService {
 
 	@Autowired
 	ClientRepository repository;
-	
+
 	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 
@@ -26,7 +28,7 @@ public class ClientService {
 
 		return list.map(x -> new ClientDTO(x));
 	}
-	
+
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 
@@ -34,5 +36,40 @@ public class ClientService {
 		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found."));
 
 		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+
+		Client entity = new Client();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+
+		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+
+		try {
+
+			Client entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+
+			return new ClientDTO(entity);
+		} catch (EntityNotFoundException e) {
+
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	private void copyDtoToEntity(ClientDTO dto, Client entity) {
+
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
 	}
 }
